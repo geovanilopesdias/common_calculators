8e4c92a62f7a1a13aa0869c7369745098cae00f3
Geovani Lopes Dias <geovani.phy.dias@gmail.com>
2023-10-18 16:17:12 -0300

    Update tkinter_calculator.py

--- tkinter_calculator.py
+++ tkinter_calculator.py
@@ -5,28 +5,30 @@ root = tk.Tk()
 
 #Root window configuration
 root.title('Gheowin Calculator')
-root.geometry('350x250')  # configura dimensões e posicionamento
+style = ttk.Style()
+root.geometry('300x270')  # configura dimensões e posicionamento
 root.resizable(False, False)  # torna a janela impossível de redimensionar
 display_padding = 10
-display_style = ttk.Style(root)
-display_style.configure('Display.TEntry', background='#90EE90')
-
 
 # Grid configuration: 4 columns x 7 rows:
 calc_label = ttk.Label(root,
                         text='Basic Calculator',
                         font=("Courier", 12))
-calc_label.grid(column=0, row = 0, columnspan=4)
+calc_label.grid(column=0, row = 0, columnspan=4, padx=display_padding, pady=display_padding)
 
-calc_display = tk.Text(root, height="1", width="16", font=("Courier", 20), state='disabled')
+calc_display = tk.Text(root, height="1", width="12", font=("Courier", 20), state='disabled')
 calc_display.tag_configure("right", justify='right')
-calc_display.grid(column=0, row = 1, columnspan=4, sticky='EW', padx=display_padding, pady=display_padding)
+calc_display.grid(column=0, row = 1, columnspan=3, sticky='EW', padx=display_padding, pady=display_padding)
 
 # Global variables
 display_content = "0"
 display_memory = float()
+last_input = float()
 last_operation = str()
-is_first_digit = True
+next_operation = str()
+is_first_operation = True
+calc_display.insert(1.0, "0.0")
+calc_display['state'] = 'disabled'
 
 
 # Definição das funções:
@@ -45,15 +47,27 @@ def set_display(value):
         calc_display['state'] = 'disabled'
 
 
-def display_clear():
+def prepare_display():
     global display_content
     global display_memory
-    global is_first_digit
+    
+    display_content = "0"
+    calc_display.delete(1.0, "end")
+    calc_display.insert(1.0, str(display_memory))
+    calc_display['state'] = 'disabled'
+
+
+def clear_display():
+    global display_content
+    global display_memory
+    global is_first_operation
 
     calc_display['state'] = 'normal'
-    display_content = "0"
     display_memory = 0
-    is_first_digit = True
+    last_input = 0
+    last_operation = str()
+    is_first_operation = True
+    display_content = "0"
     calc_display.delete(1.0, "end")
     calc_display.insert(1.0, display_content)
     calc_display['state'] = 'disabled'
@@ -62,100 +76,23 @@ def display_clear():
 def begin_calculation_for(operation):
     global display_content
     global display_memory
-    global is_first_digit
+    global is_first_operation
     global last_operation
 
     calc_display['state'] = 'normal'
     last_operation = operation
     display_memory = float(display_content)
-    is_first_digit = False
-    display_content = ""
+    is_first_operation = False
+    display_content = "0"
     calc_display.delete(1.0, "end")
     calc_display.insert(1.0, str(display_memory))
     calc_display['state'] = 'disabled'
-    
 
-def sum_up():
+
+def finish_calculation_to_begin(next_operation):
+    global last_operation
     global display_content
-    global display_memory
-    global is_first_digit
-    global last_operation
 
-    if is_first_digit:
-        begin_calculation_for("+")
-
-    else:
-        calc_display['state'] = 'normal'
-        last_operation = "+"
-        display_memory += float(display_content)
-        display_content = ""
-        calc_display.delete(1.0, "end")
-        calc_display.insert(1.0, str(display_memory))
-        calc_display['state'] = 'disabled'
-
-
-def subtract():
-    global display_content
-    global display_memory
-    global is_first_digit
-    global last_operation
-
-    if is_first_digit:
-        begin_calculation_for("-")
-
-    else:
-        calc_display['state'] = 'normal'
-        last_operation = "-"
-        display_memory -= float(display_content)
-        display_content = ""
-        calc_display.delete(1.0, "end")
-        calc_display.insert(1.0, str(display_memory))
-        calc_display['state'] = 'disabled'
-
-
-def multiply():
-    global display_content
-    global display_memory
-    global is_first_digit
-    global last_operation
-
-    if is_first_digit:
-        begin_calculation_for("*")
-
-    else:
-        calc_display['state'] = 'normal'
-        last_operation = "*"
-        display_memory *= float(display_content)
-        display_content = ""
-        calc_display.delete(1.0, "end")
-        calc_display.insert(1.0, str(display_memory))
-        calc_display['state'] = 'disabled'
-
-
-def divide():
-    global display_content
-    global display_memory
-    global is_first_digit
-    global last_operation
-
-    if is_first_digit:
-        begin_calculation_for("/")
-
-    else:
-        calc_display['state'] = 'normal'
-        last_operation = "/"
-        display_memory /= float(display_content)
-        display_content = ""
-        calc_display.delete(1.0, "end")
-        calc_display.insert(1.0, str(display_memory))
-        calc_display['state'] = 'disabled'
-
-
-def show_result():
-    global is_first_digit
-    global last_operation
-
-    calc_display['state'] = 'normal'
     if last_operation == "+":
         sum_up()
         
@@ -167,65 +104,256 @@ def show_result():
         
     elif last_operation == "/":
         divide()
-        
     else:
         pass
 
-    is_first_digit = True
-    last_operation = ""
+    last_operation = next_operation
+
+    if next_operation in ("+", "-"):
+        display_content = "0"
+    else:
+        display_content = "1"
+    
+
+def show_not_allowed_operation_msg():
+    calc_display['state'] = 'normal'
+    calc_display.delete(1.0, "end")
+    calc_display.insert(1.0, "Not allowed")
     calc_display['state'] = 'disabled'
 
 
+def sum_up():
+    global display_content
+    global display_memory
+    global is_first_operation
+    global last_operation
+    global last_input
+
+    if is_first_operation:
+        begin_calculation_for("+")
+
+    else:
+        if last_operation != "+":
+            finish_calculation_to_begin("+")
+
+        calc_display['state'] = 'normal'
+        last_operation = "+"
+        last_input = float(display_content)
+        display_memory += float(display_content)
+        prepare_display()
+
+
+def subtract():
+    global display_content
+    global display_memory
+    global is_first_operation
+    global last_operation
+    global last_input
+
+    if is_first_operation:
+        begin_calculation_for("-")
+        
+    else:
+        if last_operation != "-":
+            finish_calculation_to_begin("-")
+
+        calc_display['state'] = 'normal'
+        last_operation = "-"
+        last_input = float(display_content)
+        display_memory -= float(display_content)
+        prepare_display()
+
+
+def multiply():
+    global display_content
+    global display_memory
+    global is_first_operation
+    global last_operation
+    global last_input
+
+    if is_first_operation:
+        begin_calculation_for("*")
+
+    else:
+        if last_operation != "*":
+            finish_calculation_to_begin("*")
+                        
+        calc_display['state'] = 'normal'
+        last_operation = "*"
+        last_input = float(display_content)
+        display_memory *= float(display_content)
+        prepare_display()
+
+
+def divide():
+    global display_content
+    global display_memory
+    global is_first_operation
+    global last_operation
+    global last_input
+
+    if is_first_operation:
+        begin_calculation_for("/")
+
+    else:
+        if last_operation != "/":
+            finish_calculation_to_begin("/")
+                        
+        try:
+            calc_display['state'] = 'normal'            
+            last_operation = "/"
+            last_input = float(display_content)
+            display_memory /= float(display_content)
+            prepare_display()
+        except ZeroDivisionError:
+            show_not_allowed_operation_msg()
+
+
+def percentage():
+    global display_content
+    global display_memory
+    global last_operation
+
+    calc_display['state'] = 'normal'
+    if last_operation == "+":
+        display_memory *= (1+float(display_content)/100)
+    elif last_operation == "-":
+        display_memory *= (1-float(display_content)/100)
+    else:
+        pass
+    
+    prepare_display()
+
+
+def square_root():
+    from math import sqrt
+    global display_content
+    global display_memory
+    global is_first_operation
+
+    if display_memory < 0:    
+        show_not_allowed_operation_msg()
+
+    if is_first_operation:
+        display_memory = sqrt(float(display_content))
+        is_first_operation = False
+    else:
+        display_memory = sqrt(display_memory)
+
+    calc_display['state'] = 'normal'
+    prepare_display()
+
+
+def alter_sign():
+    global display_content
+    global display_memory
+    global is_first_operation
+
+    calc_display['state'] = 'normal'
+
+    if is_first_operation:
+        display_memory = (-1)*float(display_content)
+        is_first_operation = False
+    else:
+        display_memory *= (-1)
+        
+    prepare_display()
+
+
+def iterate_last_operation():
+    global display_content
+    global display_memory
+    global last_input
+    global is_first_operation
+    
+    if last_input != 0 and not is_first_operation:
+        try:
+            calc_display['state'] = 'normal'
+            if last_operation == "+":
+                display_memory += last_input
+            elif last_operation == "-":
+                display_memory -= last_input
+            elif last_operation == "*":
+                display_memory *= last_input
+            elif last_operation == "/":
+                display_memory /= last_input
+
+            prepare_display()
+
+        except ZeroDivisionError:
+            show_not_allowed_operation_msg()
+
+
+def show_result():
+    finish_calculation_to_begin(last_operation)
+    
+
+
 # Construção dos botões:
-zer_btn = ttk.Button(root, text='0', command=lambda: set_display(0))
+num_btn_cfg = {'bg': 'white', 'width': 4}
+ope_btn_cfg = {'bg': 'blue', 'fg': 'white', 'width': 4}
+
+zer_btn = tk.Button(root, text='0', command=lambda: set_display(0), **num_btn_cfg)
 zer_btn.grid(column=1, row=6)
 
-one_btn = ttk.Button(root, text='1', command=lambda: set_display(1))
+one_btn = tk.Button(root, text='1', command=lambda: set_display(1), **num_btn_cfg)
 one_btn.grid(column=0, row=5)
 
-two_btn = ttk.Button(root, text='2', command=lambda: set_display(2))
+two_btn = tk.Button(root, text='2', command=lambda: set_display(2), **num_btn_cfg)
 two_btn.grid(column=1, row=5)
 
-two_btn = ttk.Button(root, text='3', command=lambda: set_display(3))
+two_btn = tk.Button(root, text='3', command=lambda: set_display(3), **num_btn_cfg)
 two_btn.grid(column=2, row=5)
 
-fou_btn = ttk.Button(root, text='4', command=lambda: set_display(4))
+fou_btn = tk.Button(root, text='4', command=lambda: set_display(4), **num_btn_cfg)
 fou_btn.grid(column=0, row=4)
 
-fiv_btn = ttk.Button(root, text='5', command=lambda: set_display(5))
+fiv_btn = tk.Button(root, text='5', command=lambda: set_display(5), **num_btn_cfg)
 fiv_btn.grid(column=1, row=4)
 
-six_btn = ttk.Button(root, text='6', command=lambda: set_display(6))
+six_btn = tk.Button(root, text='6', command=lambda: set_display(6), **num_btn_cfg)
 six_btn.grid(column=2, row=4)
 
-sev_btn = ttk.Button(root, text='7', command=lambda: set_display(7))
+sev_btn = tk.Button(root, text='7', command=lambda: set_display(7), **num_btn_cfg)
 sev_btn.grid(column=0, row=3)
 
-eig_btn = ttk.Button(root, text='8', command=lambda: set_display(8))
+eig_btn = tk.Button(root, text='8', command=lambda: set_display(8), **num_btn_cfg)
 eig_btn.grid(column=1, row=3)
 
-nin_btn = ttk.Button(root, text='9', command=lambda: set_display(9))
+nin_btn = tk.Button(root, text='9', command=lambda: set_display(9), **num_btn_cfg)
 nin_btn.grid(column=2, row=3)
 
-dot_btn = ttk.Button(root, text='.', command=lambda: set_display("."))
+dot_btn = tk.Button(root, text='.', command=lambda: set_display("."), **num_btn_cfg)
 dot_btn.grid(column=0, row=6)
 
-plu_btn = ttk.Button(root, text='+', command=sum_up)
+plu_btn = tk.Button(root, text='+', command=sum_up, **ope_btn_cfg)
 plu_btn.grid(column=3, row=2)
 
-min_btn = ttk.Button(root, text='-', command=subtract)
+min_btn = tk.Button(root, text='-', command=subtract, **ope_btn_cfg)
 min_btn.grid(column=3, row=3)
 
-mul_btn = ttk.Button(root, text='x', command=multiply)
+mul_btn = tk.Button(root, text='x', command=multiply, **ope_btn_cfg)
 mul_btn.grid(column=3, row=4)
 
-div_btn = ttk.Button(root, text='/', command=divide)
+div_btn = tk.Button(root, text='\u00F7', command=divide, **ope_btn_cfg)
 div_btn.grid(column=3, row=5)
 
-ans_btn = ttk.Button(root, text='=', command=show_result)
+per_btn = tk.Button(root, text='%', command=percentage, **ope_btn_cfg)
+per_btn.grid(column=1, row=2)
+
+sqr_btn = tk.Button(root, text='\u221A', command=square_root, **ope_btn_cfg)
+sqr_btn.grid(column=2, row=2)
+
+sgn_btn = tk.Button(root, text='+/\u23BC', command=alter_sign, bg='grey', fg = '#ffffff', width=4)
+sgn_btn.grid(column=2, row=6)
+
+ans_btn = tk.Button(root, text='=', command=show_result, bg='black', fg = '#ffffff', width=4)
 ans_btn.grid(column=3, row=6)
 
-cle_btn = ttk.Button(root, text='C', command=display_clear)
+ite_btn = tk.Button(root, text='\u2B6F', command=iterate_last_operation, bg='yellow', width=4)
+ite_btn.grid(column=3, row=1)
+
+cle_btn = tk.Button(root, text='C', command=clear_display, bg='#800000', fg = '#ffffff', width=4)
 cle_btn.grid(column=0, row=2)
 
 
